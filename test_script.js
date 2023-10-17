490cb729d62b195319f9b90178577481e7e56216
ARCL01 <132585449+ARCL01@users.noreply.github.com>
2023-10-17 21:11:08 +0200

    Deleted unnecessary file (test_script.js) and added correct html test file (index.html). Main (.ino) program remained unchanged.

--- test_script.js
+++
@@ -1,8 +0,0 @@
-function send_config() {
-    var delay_conf = document.getElementById("delay_config").value; 
-    var wbh_ip = document.getElementById('wbh_ip').value; 
-    var colour = document.getElementById('colour').value; 
-    var run_value = document.getElementById('run').value; 
-    var run_value = document.getElementById('run').value;
-    window.open('http://'+ LocalIP +'config/delay=' + delay_conf + '/wbh_ip=' + wbh_ip + '/colour=' + colour + '/run=' + run_value + '/config_end','_self');
-}
